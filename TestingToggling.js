2db607da28d185268556a423ea74dc45df453abe
natestocker17 <natestocker@gmail.com>
2016-08-30 10:50:10 +1200

    Update TestingToggling.js

--- TestingToggling.js
+++ TestingToggling.js
@@ -1,5 +1,21 @@
-$('.top').on('click', function() {
-	$parent_box = $(this).closest('.box');
-	$parent_box.siblings().find('.bottom').slideUp();
-	$parent_box.find('.bottom').slideToggle(1000, 'swing');
-});
+function toggle_visibility(cl){
+   var els = document.getElementsByClassName(cl);
+   for(var i=0; i<els.length; ++i){
+      var s = els[i].style;
+      s.display = s.display==='none' ? 'block' : 'none';
+   };
+}
+
+function ToggleA(){
+  toggle_visibility('clsA');
+  }
+
+
+function ToggleB(){
+  toggle_visibility('clsB');
+  }
+  
+
+function ToggleC(){
+  toggle_visibility('clsC');
+  }
