6c9bb1580999e5cb6084378df5dd330a8b76018c
natestocker17 <natestocker@gmail.com>
2016-08-30 12:02:03 +1200

    Update TestingToggling.js

--- TestingToggling.js
+++ TestingToggling.js
@@ -6,16 +6,31 @@ function toggle_visibility(cl){
    };
 }
 
+function make_invisibile(cl){
+   var els = document.getElementsByClassName(cl);
+   for(var i=0; i<els.length; ++i){
+      var s = els[i].style;
+      s.display = 'none';
+   };
+}
+
 function ToggleA(){
   toggle_visibility('clsA');
+  make_invisibile('clsB');
+  make_invisibile('clsC');
   }
 
 
 function ToggleB(){
   toggle_visibility('clsB');
+  make_invisibile('clsA');
+  make_invisibile('clsC');
   }
   
 
 function ToggleC(){
   toggle_visibility('clsC');
+  make_invisibile('clsA');
+  make_invisibile('clsB');
+
   }
