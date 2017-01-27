8f53129f801c11782d671e7ed70bdf2fb3b93360
natestocker17 <natestocker@gmail.com>
2017-01-27 13:06:33 +1300

    Update GraphData.js

--- content/js/GraphData.js
+++ content/js/GraphData.js
@@ -1,19 +1,98 @@
 var graphdata = [{
-		name : "Development By Month",
-		id : "DevMonthInfo",
-		container : "DevelopmentbyMonth",
+		name : "Consented Dwellings and Population Growth Information",
+		id : "BCsAndPopulationGraph",
+		container : "BCsAndPopulation",
+		width : "100%",
+		height : "500px",
+		csv : "/wccgraphs/data/BCsAndPopulationGraph.csv",
+		func : "BCsAndPopulationGraph",
+		funcFile : "/wccgraphs/graphFunctions/BCsAndPopulationGraph.js",
+		link : [
+			{
+				linktext : "Datasource",
+				text : "Statistics New Zealand and Wellington City Council (.csv)",
+				url : "/wccgraphs/data/BCsAndPopulationGraph.csv"
+			}
+		]
+	}, {
+		name : "Citywide By Suburb Information",
+		id : "CitywideInfo",
+		container : "CitywideBySuburb",
+		width : "100%",
+		height : "500px",
+		csv : "/wccgraphs/data/SuburbBCDataAnnualbySuburbRanked.csv",
+		func : "CitywideBySuburbgraph",
+		funcFile : "/wccgraphs/graphFunctions/CitywideBySuburbgraph.js",
+		button : [
+			{
+				text : "Sort by Distance to CBD / Alphabetically",
+				id : "SortType"
+			}
+		],
+		drop : [
+			{
+				text : "Start year",
+				id : "dropdownStartYear_Citywide_by_Suburb",
+				valtext : ["2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004"]
+			},{
+				text : "End year",
+				id : "dropdownEndYear_Citywide_by_Suburb",
+				valtext : ["2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004"]
+			},{
+				text : "Choose a data type",
+				id : "datatypedd_CitywideBySuburbgraph",
+				valtext : ["Total Dwellings Consented","Consented Dwellings by Type"]
+			}
+		],
+		link : [
+			{
+				linktext : "Datasource",
+				text : "Statistics New Zealand and Wellington City Council (.csv)",
+				url : "/wccgraphs/data/SuburbBCDataAnnualbySuburbRanked.csv"
+			}
+		]
+	}, {
+		name : "Citywide By Suburb Consented Value Information",
+		id : "CitywideBySubConsentedValue",
+		container : "CitywideBySuburbConsentedValue",
+		width : "100%",
+		height : "500px",
+		csv : "/wccgraphs/data/SuburbBCDataAnnualbySuburbRanked.csv",
+		func : "CitywideBySuburbConsentedValuegraph",
+		funcFile : "/wccgraphs/graphFunctions/CitywideBySuburbConsentedValuegraph.js",
+ 		button : [
+			{
+				text : "Sort by Distance to CBD / Alphabetically",
+				id : "SortType"
+			}
+		],
+		drop : [
+			{
+				text : "Start year",
+				id : "dropdownStartYear_Citywide_by_Suburb",
+				valtext : ["2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004"]
+			},{
+				text : "End year",
+				id : "dropdownEndYear_Citywide_by_Suburb",
+				valtext : ["2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004"]
+			}
+		],
+		link : [
+			{
+				linktext : "Datasource",
+				text : "Statistics New Zealand and Wellington City Council (.csv)",
+				url : "/wccgraphs/data/SuburbBCDataAnnualbySuburbRanked.csv"
+			}
+		]
+	}, {
+		name : "Consented Value By Month Information",
+		id : "ConsentedValueByMonthInfo",
+		container : "ConsentedValuebyMonth",
 		width : "100%",
 		height : "500px",
 		csv : "/wccgraphs/data/CitywideBCdataMonthly.csv",
-		func : 'DevelopmentbyMonthgraph',
-		funcFile : "/wccgraphs/graphFunctions/DevByMonth.js",
-		drop : [
-			{
-				text : "Choose a data type",
-				id : "datatypedd_DevelopmentbyMonthgraph",
-				valtext : ["Total Dwellings Consented","Consented Dwellings by Type","Value of Consented Construction"]
-			}
-		],
+		func : 'ConsentedValuebyMonthgraph',
+		funcFile : "/wccgraphs/graphFunctions/ConsentedValuebyMonthgraph.js",
 		link : [
 			{
 				linktext : "Datasource",
@@ -22,14 +101,14 @@ var graphdata = [{
 			}
 		]
 	}, {
-		name : "Suburb Information",
-		id : "SubInfo",
-		container : "SuburbInformation",
+		name : "Consented Value by Suburb Information",
+		id : "ConsentedValueBySuburbInfo",
+		container : "SuburbConsentedValue",
 		width : "100%",
 		height : "500px",
 		csv : "/wccgraphs/data/SuburbBCDataAnnualbyYear.csv",
-		func : "graphmaker_suburbgraph",
-		funcFile : "/wccgraphs/graphFunctions/SuburbGraph.js",
+		func : "ConsentedValuebySuburbGraph",
+		funcFile : "/wccgraphs/graphFunctions/ConsentedValuebySuburbGraph.js",
 		drop : [
 			{
 				text : "Choose an area",
@@ -51,10 +130,6 @@ var graphdata = [{
 				"Newlands","Newtown","Ngaio","Ngauranga","Northland","Ohariu","Oriental Bay","Owhiro Bay","Paparangi","Pipitea","Rongotai",
 				"Roseneath","Seatoun","Southgate","Strathmore Park","Takapu Valley","Tawa","Te Aro","Thorndon","Vogeltown","Wadestown",
 				"Wellington Central","Wilton","Woodridge"]
-			},{
-				text : "Choose a data type",
-				id : "datatypedd_suburbgraph",
-				valtext : ["Total Dwellings Consented","Consented Dwellings by Type","Value of Consented Construction"]
 			}
 		],
 		link : [
@@ -65,56 +140,81 @@ var graphdata = [{
 			}
 		]
 	}, {
-		name : "Citywide By Suburb Information",
-		id : "CitywideInfo",
-		container : "CitywideBySuburb",
+		name : "Development By Month",
+		id : "DevMonthInfo",
+		container : "DevelopmentbyMonth",
 		width : "100%",
 		height : "500px",
-		csv : "/wccgraphs/data/SuburbBCDataAnnualbySuburbRanked.csv",
-		func : "CitywideBySuburbgraph",
-		funcFile : "/wccgraphs/graphFunctions/CitywideBySuburb.js",
-		button : [
-			{
-				text : "Sort by Distance to CBD / Alphabetically",
-				id : "SortType"
-			}
-		],
+		csv : "/wccgraphs/data/CitywideBCdataMonthly.csv",
+		func : 'DevelopmentbyMonthgraph',
+		funcFile : "/wccgraphs/graphFunctions/DevelopmentbyMonthgraph.js",
 		drop : [
 			{
-				text : "Start year",
-				id : "dropdownStartYear_Citywide_by_Suburb",
-				valtext : ["2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004"]
-			},{
-				text : "End year",
-				id : "dropdownEndYear_Citywide_by_Suburb",
-				valtext : ["2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004"]
-			},{
 				text : "Choose a data type",
-				id : "datatypedd_CitywideBySuburbgraph",
-				valtext : ["Total Dwellings Consented","Consented Dwellings by Type","Value of Consented Construction"]
+				id : "datatypedd_DevelopmentbyMonthgraph",
+				valtext : ["Total Dwellings Consented","Consented Dwellings by Type"]
 			}
 		],
 		link : [
 			{
 				linktext : "Datasource",
 				text : "Statistics New Zealand and Wellington City Council (.csv)",
-				url : "/wccgraphs/data/SuburbBCDataAnnualbySuburbRanked.csv"
+				url : "/wccgraphs/data/CitywideBCdataMonthly.csv"
+			}
+		]
+	}, {
+		name : "Development By Year",
+		id : "DevTrendsByYear",
+		container : "Dev_Trends_by_Year",
+		width : "100%",
+		height : "500px",
+		csv : "/wccgraphs/data/CitywideBCdataAnnual.csv",
+		func : 'DevTrendsByYearGraph',
+		funcFile : "/wccgraphs/graphFunctions/DevTrendsByYearGraph.js",
+		link : [
+			{
+				linktext : "Datasource",
+				text : "Statistics New Zealand and Wellington City Council (.csv)",
+				url : "/wccgraphs/data/CitywideBCdataAnnual.csv"
+			}
+		]
+	}, {
+		name : "Dwellings Completed",
+		id : "DwellingsCCCd",
+		container : "DwellingsCompleted",
+		width : "100%",
+		height : "500px",
+		csv : "/wccgraphs/data/DwellingsCCCd.csv",
+		func : 'DwellingsCCCd',
+		funcFile : "/wccgraphs/graphFunctions/DwellingsCCCd.js",
+		drop : [
+			{
+				text : "Choose an extra dataset",
+				id : "DwellingsCCCd_ExtraDataType",
+				valtext : ["-","Consented Dwellings","Projected Dwellings"]
+			}
+		],
+		link : [
+			{
+				linktext : "Datasource",
+				text : "Statistics New Zealand and Wellington City Council (.csv)",
+				url : "/wccgraphs/data/DwellingsCCCd.csv"
 			}
 		]
 	}, {
 		name : "Infometrics Graph",
-		id : "InfometricsInfo",
-		container : "InfometricsGraph",
+		id : "InfometricsGraph",
+		container : "InfometricsInfo",
 		width : "100%",
 		height : "500px",
 		csv : "/wccgraphs/data/Infometrics.csv",
 		func : "InfometricsGraph",
-		funcFile : "/wccgraphs/graphFunctions/Isometrics.js",
+		funcFile : "/wccgraphs/graphFunctions/InfometricsGraph.js",
 		drop : [
 			{
 				text : "Choose a data type",
 				id : "InfometricsGraph_Datatype",
-				valtext : ["Housing Affordability Index","House Price Growth","Rental Affordability Index","Growth of Average Rental Costs"]
+				valtext : ["Housing Affordability Index","Rental Affordability Index"]
 			}
 		],
 		link : [
@@ -133,5 +233,167 @@ var graphdata = [{
 				id : "ExtraReferences"
 			}
 		]
-	}
+	}, {
+		name : "Dwelling Type Over Time",
+		id : "DwellingTypePie",
+		container : "DwellingTypePieGraph",
+		width : "100%",
+		height : "500px",
+		csv : "/wccgraphs/data/DwellingTypeSplitAnnual.csv",
+		func : 'piegraphmaker',
+		funcFile : "/wccgraphs/graphFunctions/Piegraphmaker.js",
+		drop : [
+			{
+				text : "Start year",
+				id : "dropdownStartYear_Pie",
+				valtext : ["2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000","1999","1998","1997","1996","1995","1994","1993","1992","1991"]
+			},{
+				text : "End year",
+				id : "dropdownEndYear_Pie",
+				valtext : ["2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000","1999","1998","1997","1996","1995","1994","1993","1992","1991"]
+			},{
+				text : "Comparison start year",
+				id : "dropdownStartYearTwo_Pie",
+				valtext : ["-","2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000","1999","1998","1997","1996","1995","1994","1993","1992","1991"]
+			},{
+				text : "Comparison end year",
+				id : "dropdownEndYearTwo_Pie",
+				valtext : ["-","2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000","1999","1998","1997","1996","1995","1994","1993","1992","1991"]
+			}
+		],
+		link : [
+			{
+				linktext : "Datasource",
+				text : "Statistics New Zealand and Wellington City Council (.csv)",
+				url : "/wccgraphs/data/DwellingTypeSplitAnnual.csv"
+			}
+		]
+	}, {
+		name : "Property Data Graph",
+		id : "PropertyDataGraph",
+		container : "PropertyDataInfo",
+		width : "100%",
+		height : "500px",
+		csv : "/wccgraphs/data/HouseDataQuarterly.csv",
+		func : 'PropertyDataGraph',
+		funcFile : "/wccgraphs/graphFunctions/PropertyDataGraph.js",
+		drop : [
+			{
+				text : "Choose a data type",
+				id : "PropertySales_DataType",
+				valtext : ["Median House Price","Number of House Sales","Both"]
+			}
+		],		
+		link : [
+			{
+				linktext : "Datasource",
+				text : "Statistics New Zealand and Wellington City Council (.csv)",
+				url : "/wccgraphs/data/HouseDataQuarterly.csv"
+			}
+		]
+	}, {
+		name : "Regional Info Graph",
+		id : "RegionalByYearGraph",
+		container : "RegionalByYear",
+		width : "100%",
+		height : "500px",
+		csv : "/wccgraphs/data/RegionalByYear.csv",
+		func : 'RegionalByYearGraph',
+		funcFile : "/wccgraphs/graphFunctions/RegionalByYear.js",
+		drop : [
+			{
+				text : "Choose a data type",
+				id : "Regional_DataType",
+				valtext : ["Total Dwellings","Houses","Units","Apartments","Retirement village units"]
+			}
+		],		link : [
+			{
+				linktext : "Datasource",
+				text : "Statistics New Zealand and Wellington City Council (.csv)",
+				url : "/wccgraphs/data/RegionalByYear.csv"
+			}
+		]
+	}, {
+		name : "Rental Graph",
+		id : "RentalGraph",
+		container : "RentGraph",
+		width : "100%",
+		height : "500px",
+		csv : "/wccgraphs/data/RentalData.csv",
+		func : 'RentalGraph',
+		funcFile : "/wccgraphs/graphFunctions/RentalGraph.js",
+		drop : [
+			{
+				text : "Choose a data type",
+				id : "TypeDD_RentalGraph",
+				valtext : ["Wellington City", "Mean Rent Comparison"]
+			}
+		],	
+		link : [
+			{
+				linktext : "Datasource",
+				text : "Statistics New Zealand and Wellington City Council (.csv)",
+				url : "/wccgraphs/data/RentalData.csv"
+			}
+		]
+	}, {
+		name : "Resource Consents Graph",
+		id : "ResourceConsentsGraph",
+		container : "ResourceConsentsIssued",
+		width : "100%",
+		height : "500px",
+		csv : "/wccgraphs/data/ResourceConsentsMonthly.csv",
+		func : 'ResourceConsentsIssuedgraph',
+		funcFile : "/wccgraphs/graphFunctions/ResourceConsentsIssuedGraph.js",
+		link : [
+			{
+				linktext : "Datasource",
+				text : "Statistics New Zealand and Wellington City Council (.csv)",
+				url : "/wccgraphs/data/ResourceConsentsMonthly.csv"
+			}
+		]
+	}, {
+		name : "Suburb Information",
+		id : "SubInfo",
+		container : "SuburbInformation",
+		width : "100%",
+		height : "500px",
+		csv : "/wccgraphs/data/SuburbBCDataAnnualbyYear.csv",
+		func : "SuburbAnnualDataGraph",
+		funcFile : "/wccgraphs/graphFunctions/SuburbAnnualDataGraph.js",
+		drop : [
+			{
+				text : "Choose an area",
+				id : "datafromdropdown_suburbgraph",
+				valtext : ["Wellington City Total","Aro Valley","Berhampore","Breaker Bay","Broadmeadows","Brooklyn","Churton Park",
+				"Crofton Downs","Glenside","Grenada North","Grenada Village","Hataitai","Highbury","Horokiwi","Houghton Bay",
+				"Island Bay","Johnsonville","Kaiwharawhara","Karaka Bays","Karori","Kelburn","Khandallah","Kilbirnie","Kingston",
+				"Lyall Bay","Makara","Makara Beach","Maupuia","Melrose","Miramar","Moa Point","Mornington","Mt Cook","Mt Victoria",
+				"Newlands","Newtown","Ngaio","Ngauranga","Northland","Ohariu","Oriental Bay","Owhiro Bay","Paparangi","Pipitea","Rongotai",
+				"Roseneath","Seatoun","Southgate","Strathmore Park","Takapu Valley","Tawa","Te Aro","Thorndon","Vogeltown","Wadestown",
+				"Wellington Central","Wilton","Woodridge"]
+			},{
+				text : "Compare with another area",
+				id : "comparisiondd_suburbgraph",
+				valtext : [" ","Wellington City Total","Aro Valley","Berhampore","Breaker Bay","Broadmeadows","Brooklyn","Churton Park",
+				"Crofton Downs","Glenside","Grenada North","Grenada Village","Hataitai","Highbury","Horokiwi","Houghton Bay",
+				"Island Bay","Johnsonville","Kaiwharawhara","Karaka Bays","Karori","Kelburn","Khandallah","Kilbirnie","Kingston",
+				"Lyall Bay","Makara","Makara Beach","Maupuia","Melrose","Miramar","Moa Point","Mornington","Mt Cook","Mt Victoria",
+				"Newlands","Newtown","Ngaio","Ngauranga","Northland","Ohariu","Oriental Bay","Owhiro Bay","Paparangi","Pipitea","Rongotai",
+				"Roseneath","Seatoun","Southgate","Strathmore Park","Takapu Valley","Tawa","Te Aro","Thorndon","Vogeltown","Wadestown",
+				"Wellington Central","Wilton","Woodridge"]
+			},{
+				text : "Choose a data type",
+				id : "datatypedd_suburbgraph",
+				valtext : ["Total Dwellings Consented","Consented Dwellings by Type"]
+			}
+		],
+		link : [
+			{
+				linktext : "Datasource",
+				text : "Statistics New Zealand and Wellington City Council (.csv)",
+				url : "/wccgraphs/data/SuburbBCDataAnnualbyYear.csv"
+			}
+		]
+		}
 ];
