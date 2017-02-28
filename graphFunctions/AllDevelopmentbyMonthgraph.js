03d83ede1d1c2b6a9e6ac65af6b389bcfb834dbb
natestocker17 <natestocker@gmail.com>
2017-02-28 14:04:29 +1300

    Update AllDevelopmentbyMonthgraph.js

--- graphFunctions/AllDevelopmentbyMonthgraph.js
+++ graphFunctions/AllDevelopmentbyMonthgraph.js
@@ -29,7 +29,7 @@ else {
 
 
 
-if (datatypetouse_Development_by_Month == "" || datatypetouse_Development_by_Month == "Total Dwellings Consented") {
+if (datatypetouse_Development_by_Month == "Total Dwellings Consented") {
 
 	MonthsLength = $.map(chartData, function(data){return data["Citywide Months Longterm Months"];}).length
 
@@ -52,7 +52,7 @@ if (datatypetouse_Development_by_Month == "" || datatypetouse_Development_by_Mon
 		}
 	;}
 
-else {
+else if (datatypetouse_Development_by_Month == "Consented Dwellings by Type") {
 
 	data = [{
 		x: $.map(chartData, function(data){
@@ -122,7 +122,129 @@ else {
 			barmode: 'stack'
 			}
 	;}
+	
+else if (datatypetouse_Development_by_Month == "Value of Consented Construction") {
 
+	data = [{
+		x: $.map(chartData, function(data){
+			return data["Citywide Months Longterm Months"];
+		}),
+		y: $.map(chartData, function(data){
+			return data[TimeframeConverted+"Residential Buildings"];
+		}),
+		name: "Residential Buildings",
+		type: 'bar',
+		marker: {color: Colour1}
+		
+        },{
+		x: $.map(chartData, function(data){
+			return data["Citywide Months Longterm Months"];
+		}),
+		y: $.map(chartData, function(data){
+			return data[TimeframeConverted+"NonResidential Construction"];
+		}),
+		name: "NonResidential Construction",
+		type: 'bar',
+		marker: {color: Colour2}
+		
+        },{
+		x: $.map(chartData, function(data){
+			return data["Citywide Months Longterm Months"];
+		}),
+		y: $.map(chartData, function(data){
+			return data[TimeframeConverted+"Total Construction Value"];
+		}),
+		name: "Total Construction Value",
+		type: 'line',
+		marker: {color: Colour3}
+		
+        }];
 
+	MonthsLength = $.map(chartData, function(data){return data["Citywide Months Longterm Months"];}).length 
+	
+	layout = {
+		title: 'Consented Dwellings by Type and Month',
+		showlegend: true,
+//		legend: {x: 0.8, y: 0.9},
+		legend : {orientation: 'h'},
+		xaxis: {
+			zeroline: false, 
+			tickmode: 'linear',
+			tick0: 0,
+			dtick: 2,
+			tickangle: 22.5, 
+			fixedrange: Zooming, 
+			range: [MonthsLength-24.6,MonthsLength]
+			},
+		yaxis: {
+			zeroline: false, 
+			fixedrange: Zooming, 
+			range: [0,160]},
+			barmode: 'stack'
+			}
+	;}
+
+else {
+
+	data = [{
+		x: $.map(chartData, function(data){
+			return data["Citywide Months Longterm Months"];
+		}),
+		y: $.map(chartData, function(data){
+			return data[TimeframeConverted+"Land Use Consents"];
+		}),
+		name: "Land Use Consents",
+		type: 'bar',
+		marker: {color: Colour1}
+		
+        },{
+		x: $.map(chartData, function(data){
+			return data["Citywide Months Longterm Months"];
+		}),
+		y: $.map(chartData, function(data){
+			return data[TimeframeConverted+"Subdivision Consents"];
+		}),
+		name: "Subdivision Consents",
+		type: 'bar',
+		marker: {color: Colour2}
+		
+        },{
+		x: $.map(chartData, function(data){
+			return data["Citywide Months Longterm Months"];
+		}),
+		y: $.map(chartData, function(data){
+			return data[TimeframeConverted+"Resource Consents"];
+		}),
+		name: "Total Resource Consents",
+		type: 'line',
+		marker: {color: Colour3}
+		
+        }];
+
+	MonthsLength = $.map(chartData, function(data){return data["Citywide Months Longterm Months"];}).length 
+	
+	layout = {
+		title: 'Consented Dwellings by Type and Month',
+		showlegend: true,
+//		legend: {x: 0.8, y: 0.9},
+		legend : {orientation: 'h'},
+		xaxis: {
+			zeroline: false, 
+			tickmode: 'linear',
+			tick0: 0,
+			dtick: 2,
+			tickangle: 22.5, 
+			fixedrange: Zooming, 
+			range: [MonthsLength-24.6,MonthsLength]
+			},
+		yaxis: {
+			zeroline: false, 
+			fixedrange: Zooming, 
+			range: [0,160]},
+			barmode: 'stack'
+			}	
+	;}
+
+	
 Plotly.newPlot('AllDevelopmentbyMonth', data, layout);
 }
